0b70cde80632ab3eda52692f9cf3f4d4d2c555f2
Roaa Soloh <solohrk@rhu.edu.lb>
2024-12-03 18:55:47 +0200

    new changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'check_student' => App\Http\Middleware\CheckStudent::class
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
