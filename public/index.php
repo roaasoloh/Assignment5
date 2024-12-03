043dd5608a54f383bd136247f4554908d39b865d
Roaa Soloh <solohrk@rhu.edu.lb>
2024-12-03 19:48:46 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,21 +1,17 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell
- */
+use Illuminate\Http\Request;
 
-/** Detect the application environment */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+define('LARAVEL_START', microtime(true));
 
-/** Run the application */
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-$response->send();
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
